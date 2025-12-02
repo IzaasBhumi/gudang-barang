6484dea53e1e5b12dedfbfc6b77e0c628648a791
IzaasBhumi <izaasbhumij59gmail.com>
2025-12-02 12:06:05 +0700

    test

---
+++ resources/views/kategori-produk/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.kai')
+@section('content')
+    <div class="card">
+        <div class="card-body py-5"></div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\KategoriProdukController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function(){
+    Route::prefix('master-data')->name('master-data. ')->group(function(){
+        Route::resource('kategori-produk', KategoriProdukController::class);
+    });
+});
